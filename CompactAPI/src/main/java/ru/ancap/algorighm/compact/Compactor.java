74f8fccf26e00266f82ad98c56acf8c4503aa242
ancap-kun <vovanchikputin@vk.com>
2023-03-02 23:10:31 +0500

    1.1

---
+++ CompactAPI/src/main/java/ru/ancap/algorighm/compact/Compactor.java
@@ -0,0 +1,20 @@
+package ru.ancap.algorighm.compact;
+
+/**
+ * Definitely supports packing range from Integer.MIN_VALUE + 1 to Integer.MAX_VALUE by specification. 
+ * <b>May</b> support range from Integer.MIN_VALUE to Integer.MAX_VALUE.
+ */
+public interface Compactor {
+    
+    int MIN_VALUE = -2147483647;
+    int MAX_VALUE =  2147483647;
+    
+    int[] unpack(long code);
+    
+    long pack(int first, int second);
+    
+    default long pack(long first, long second) {
+        return this.pack((int) first, (int) second);
+    }
+    
+}
---
+++ WalkthroughAPI/src/main/java/ru/ancap/algorithm/walkthrough/classic/PIRWalkthroughOperator.java
@@ -0,0 +1,30 @@
+package ru.ancap.algorithm.walkthrough.classic;
+
+import org.locationtech.jts.geom.*;
+
+import java.util.function.Function;
+
+/**
+ * PolygonIntersectsRegionWalkthroughOperator
+ */
+public class PIRWalkthroughOperator<NODE> extends RegionWalkthroughOperator<NODE> {
+
+    public PIRWalkthroughOperator(java.awt.Polygon inputRegion, Function<NODE, java.awt.Polygon> polygonFunction) {
+        super((node) -> {
+            java.awt.Polygon inputPolygon = polygonFunction.apply(node);
+            
+            GeometryFactory geometryFactory = new GeometryFactory();
+            Polygon region = jtsPolygonOfAwt(geometryFactory, inputRegion);
+            Polygon polygon = jtsPolygonOfAwt(geometryFactory, inputPolygon);
+
+            return region.intersection(polygon).relate(geometryFactory.createPoint()).isIntersects();
+        });
+    }
+
+    private static Polygon jtsPolygonOfAwt(GeometryFactory geometryFactory, java.awt.Polygon awtRegion) {
+        Coordinate[] coordinates = new Coordinate[awtRegion.npoints];
+        for (int i = 0; i < awtRegion.npoints; i++) coordinates[i] = new Coordinate(awtRegion.xpoints[i], awtRegion.ypoints[i]);
+        return geometryFactory.createPolygon(coordinates);
+    }
+
+}
