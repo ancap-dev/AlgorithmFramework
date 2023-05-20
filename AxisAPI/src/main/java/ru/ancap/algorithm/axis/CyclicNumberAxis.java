d4b4927b00f65ac1a8fc26df016acdb18535616a
ancap-kun <vovanchikputin@vk.com>
2023-05-21 04:37:31 +0500

    1.4

--- AxisAPI/src/main/java/ru/ancap/algorithm/axis/CyclicNumberAxis.java
+++ AxisAPI/src/main/java/ru/ancap/algorithm/axis/CyclicNumberAxis.java
@@ -4,29 +4,29 @@ public class CyclicNumberAxis implements NumberAxis {
 
     public static CyclicNumberAxis HEXAGONAL = new CyclicNumberAxis(6);
 
-    private final int boundOffset;
-    private final int period;
+    private final double boundOffset;
+    private final double period;
 
-    public CyclicNumberAxis(int period) {
+    public CyclicNumberAxis(double period) {
         this(0, period, null);
     }
 
-    public CyclicNumberAxis(int start, int end) {
+    public CyclicNumberAxis(double start, double end) {
         this(start, end - start, null);
     }
 
-    private CyclicNumberAxis(int boundOffset, int period, Object signature) {
+    private CyclicNumberAxis(double boundOffset, double period, Object hack_to_add_other_method_with_the_same_signature) {
         if (period < 0) throw new IllegalArgumentException("Period can not be smaller than 0");
         this.boundOffset = boundOffset;
         this.period = period;
     }
 
-    public int offset(int base, int steps) {
+    public double offset(double base, double steps) {
         if (base+1 > this.period) throw new IllegalArgumentException("Base can not be outside the borders!");
 
-        int offset = steps % this.period;
+        double offset = steps % this.period;
 
-        int newBase = base + offset;
+        double newBase = base + offset;
         if (newBase >= this.period) newBase -= this.period;
         else if (newBase < 0) newBase += this.period;
 
